a8282501c7fbfa11bdf50175f54b6d47048e7129
Peter Applebee <peterapplebee@gmail.com>
2014-08-16 13:54:29 +0100

    X to make him pose and instructions

--- js/app.js
+++ js/app.js
@@ -23,10 +23,35 @@ jQuery(document).ready(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
+
+	$(document).keydown(function(event) {
+		if (event.which == 88) {
+			playCool();
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-cool').show();
+		}
+	})
+	.keyup(function(event) {
+		if (event.which == 88) {
+			pauseCool();
+			$('.ryu-cool').hide();
+			$('.ryu-still').show();
+		}
+	});
 });
 
 function playHadouken() {
 	$('#hadouken-sound')[0].volume = 0.5;
 	$('#hadouken-sound')[0].load();
 	$('#hadouken-sound')[0].play();
+}
+
+function playCool() {
+	$('#cool-sound')[0].play();
+}
+
+function pauseCool() {
+	$('#cool-sound')[0].pause();
+	$('#cool-sound')[0].load();
 }
