111c541eaddd2fe27eca3351daf1cdca1f68af62
Peter Applebee <peterapplebee@gmail.com>
2014-08-16 12:55:19 +0100

    Added JQuery

---
+++ js/app.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function() {
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function() {
+		playHadouken();
+		console.log('mousedown');
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show()
+		.animate({'left':'300px'}, 500, function() {
+				$(this).hide();
+				$(this).css('left','-150px');
+			});
+	})
+	.mouseup(function() {
+		console.log('mouseup');
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	});
+});
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
